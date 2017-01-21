0a7e2e190df4ff8ceb12cd17cce293861a1ca5e4
Matthieu Baty <matthieubaty@gmail.com>
2017-01-21 13:40:18 +0100

    Merge fix

--- Assets/Scripts/TorpedoManager.cs
+++ Assets/Scripts/TorpedoManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class TorpedoManager : MonoBehaviour {
 
-    public Rigidbody2D Torpedo;
+    public GameObject Torpedo;
     public float b_inf_x = -5f;
     public float b_sup_x = 5f;
     public float b_inf_y = 0f;
@@ -35,8 +35,8 @@ public class TorpedoManager : MonoBehaviour {
             float rand_x = random_borne_x();
             int fact = rand_x > 0 ? 1 : -1;
             int fact2 = rand_x > 0 ? 0 : 1;
-            Rigidbody2D torpedo_instance = Instantiate(Torpedo, new Vector3(rand_x, random_borne_y(), 0), Quaternion.Euler(0, fact2 * 180, 0));
-            torpedo_instance.velocity = new Vector3(-fact * speed, 0, 0);
+            GameObject torpedo_instance = Instantiate(Torpedo, new Vector3(rand_x, random_borne_y(), 0), Quaternion.Euler(0, fact2 * 180, 0));
+            torpedo_instance.GetComponent<Rigidbody2D>().velocity = new Vector3(-fact * speed, 0, 0);
         }
 	}
 }
