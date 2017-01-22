d7837ed9a375d284fdf99b2b76644cdbd552d60b
Unknown <guillaume.hitier@epitech.eu>
2017-01-22 10:58:47 +0100

    Added mines + assets
    + changed sub

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -2,9 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class playerMovement : MonoBehaviour
 {
     public GameObject Score;
+    // public AudioClip[] soundfx;
     public float acceleration_x = 10f;
     public float acceleration_y = 8f;
     public float drag_x = 1.5f;
@@ -22,7 +23,6 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // velocity += acceleration - drag
         Rigidbody2D rb = this.GetComponent<Rigidbody2D>();
         rb.velocity = new Vector2(rb.velocity.x + (Input.GetAxis("Horizontal") * acceleration_x - drag_x * rb.velocity.x) * Time.deltaTime, rb.velocity.y + (Input.GetAxis("Vertical") * acceleration_y - drag_y * rb.velocity.y) * Time.deltaTime);
         angular_speed += Time.deltaTime * (Input.GetAxis("Vertical") * angular_acceleration - angular_speed * angular_drag);
@@ -35,6 +35,7 @@ public class PlayerMovement : MonoBehaviour
     void OnTriggerEnter2D(Collider2D coll)
     {
         Debug.Log("Boom!!!");
+// AudioSource.PlayClipAtPoint(soundfx[0], this.transform.position);
         Application.LoadLevel("gameOver");
         Score.GetComponent<Score>().setCounting(false);
     }
